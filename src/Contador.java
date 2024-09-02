dcb61f0f261ddab1ddec479744436143f4f01a3b
degrandelucas <lucasdegrande15@gmail.com>
2024-09-02 07:53:16 -0300

    closing object entradaDados of class Scanner with finally and new try

--- src/Contador.java
+++ src/Contador.java
@@ -1,47 +1,59 @@
 import java.util.Scanner; // Importar a classe Scanner do pacote java.util
-
 /**
- * A classe Contador recebe dois números de entrada via terminar pelo usuário.
- * <p>Ela contém métodos para registrar entrada, saída e análise para verificar se o segundo número é maior que o primeiro número (pré requisito),
- * caso contrário irá lançar um erro que é tratado no método main
+ * A classe <code>Contador</code> é responsável por receber dois números do usuário e calcular a diferença entre eles.
+ * <p>Se o primeiro número for maior ou igual ao segundo número, ou se os números forem iguais, uma exceção será lançada.
+ * O método principal da classe lida com a entrada do usuário, realiza o cálculo usando a classe <code>Diferenca</code>, e imprime a sequência de números.
  * 
  * @author Lucas Degrande
- * @version 1.0
+ * @version 2.0
  * @since 2024-08-31
  * 
  */
 
-public class Contador {    
+public class Contador {
+    
+    /**
+     * O método principal da classe. Recebe dois números do usuário, calcula a diferença entre eles usando a classe <code>Diferenca</code>,
+     * e imprime a sequência de números do 1 até o resultado da diferença.
+     * <p>Se houver uma exceção durante o cálculo, ela será tratada e uma mensagem de erro será exibida.
+     * 
+     * @param args Argumentos da linha de comando. Não são utilizados neste método.
+     * @throws Exception Se ocorrer um erro genérico durante a execução.
+     */
     public static void main(String[] args) throws Exception {
 
         Scanner entradaDados = new Scanner(System.in);
 
-        System.out.println("Olá, iremos realizar a contagem do primeiro número que digitar, até o segundo número.");
-        System.out.println("-------------------------------------------------------------------------------------");
-        System.out.print("Digite o 1º Número: ");
-        int numero1 = entradaDados.nextInt();
-        System.out.print("Digite o 2º Número: ");
-        int numero2 = entradaDados.nextInt();
-
-        Diferenca diferenca = new Diferenca();
-
-        int resultado = 0;
-
         try {
-            resultado = diferenca.calcular(numero1, numero2);
+            System.out.println("Olá, iremos realizar a contagem do primeiro número que digitar, até o segundo número.");
+            System.out.println("-------------------------------------------------------------------------------------");
+            System.out.print("Digite o 1º Número: ");
+            int numero1 = entradaDados.nextInt();
+            System.out.print("Digite o 2º Número: ");
+            int numero2 = entradaDados.nextInt();
 
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println("Erro de inserção: " + e.getMessage());
+            Diferenca diferenca = new Diferenca();
 
-            // Lançando uma exceção genérica diretamente no main
-            throw new Exception("Ocorreu um erro no método main");
-        }
+            int resultado = 0;
 
-        System.out.println("A diferença entre os números é: " + resultado);
-        System.out.println("Sequência de 1 até " + resultado +": ");
-        for(int i = 1; i <= resultado; i++){
-            System.out.println(i);
+            try {
+                resultado = diferenca.calcular(numero1, numero2);
+
+            } catch (Exception e) {
+                // TODO: handle exception
+                System.out.println("Erro de inserção: " + e.getMessage());
+
+                // Lançando uma exceção genérica diretamente no main
+                throw new Exception("Ocorreu um erro no método main");
+            }
+
+            System.out.println("A diferença entre os números é: " + resultado);
+            System.out.println("Sequência de 1 até " + resultado +": ");
+            for(int i = 1; i <= resultado; i++){
+                System.out.println(i);
+            }
+        } finally{
+            entradaDados.close();
         }
     }
 }
--- src/Diferenca.java
+++ src/Diferenca.java
@@ -1,5 +1,21 @@
+/**
+ * A classe <code>Diferenca</code> é responsável por calcular a diferença entre dois números.
+ * <p>Se o primeiro número for maior que o segundo número, ou se os números forem iguais, uma exceção será lançada.
+ * 
+ * @author Lucas Degrande
+ * @version 1.0
+ * @since 2024-08-31
+ */
 public class Diferenca {
-
+    /**
+     * Calcula a diferença entre dois números.
+     * <p>Se o primeiro número for maior que o segundo número, ou se os números forem iguais, uma exceção será lançada.
+     * 
+     * @param numero1 Primeiro número.
+     * @param numero2 Segundo número.
+     * @return A diferença entre <code>numero2</code> e <code>numero1</code>.
+     * @throws IllegalArgumentException Se <code>numero1</code> for maior que <code>numero2</code> ou se forem iguais.
+     */
     public int calcular(int numero1, int numero2){
 
         if (numero1 > numero2){
